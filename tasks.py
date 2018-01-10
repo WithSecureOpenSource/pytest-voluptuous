1edf52c5149cb8292f494134082995d7c62ed272
Tuukka Mustonen <tuukka.mustonen@gmail.com>
2018-01-10 23:34:53 +0200

    Dev: Update tasks

--- tasks.py
+++ tasks.py
@@ -7,60 +7,61 @@ from invoke.tasks import task
 @task
 def clean(ctx):
     """Cleans temporary artifacts of project."""
-    ignores = re.match('#\sno-clean:\s(.*)', open('.gitignore').readline()).group(1).split(' ')
+    ignores = re.match(r'^#\sno-clean:\s(.*)$', open('.gitignore').readline()).group(1).split(' ')
     ctx.run('git clean -f -X -d {}'.format(' '.join(['-e \!' + re.escape(ign) for ign in ignores])))
 
 
 @task
 def develop(ctx):
     """Start development mode."""
-    ctx.run("pip install -e .")
+    ctx.run('pip install -e .')
 
 
 @task
 def undevelop(ctx):
     """Stop development mode."""
-    ctx.run("python setup.py develop --uninstall")
+    ctx.run('python setup.py develop --uninstall')
+
+
+@task
+def analyze(ctx):
+    """Run static analysis."""
+    ctx.run('pylint pytest_voluptuous')
 
 
 @task
 def test(ctx):
     """Run unit tests."""
-    ctx.run("pytest")
+    ctx.run('pytest')
 
 
 @task
 def package(ctx):
     """Packages module."""
-    ctx.run("python setup.py bdist_wheel")
+    ctx.run('python setup.py bdist_wheel')
 
 
 @task
 def tox(ctx):
     """Run tox."""
-    ctx.run("tox")
+    ctx.run('tox')
 
 
 @task
 def release(ctx, version):
     """Releases a new version of the library."""
-    ctx.run('git tag -a {}'.format(version))
-    clean(ctx)
-    upload(ctx)
+    ctx.run('git tag -a {0} -m "Release {0}"'.format(version))
     ctx.run('git push')
     ctx.run('git push --tags')
-
-
-@task
-def register(ctx):
-    """Registers package to PyPI."""
-    ctx.run('python setup.py register')
+    clean(ctx)
+    package(ctx)
+    upload(ctx)
 
 
 @task
 def upload(ctx):
     """Uploads packages to PyPI."""
-    ctx.run('python setup.py bdist_wheel upload')
+    ctx.run('twine upload dist/pytest_voluptuous-*.whl')
 
 
-ns = Collection(clean, develop, undevelop, test, tox, package, release, register, upload)
+ns = Collection(clean, develop, undevelop, analyze, test, tox, package, release, upload)
