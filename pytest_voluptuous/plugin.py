60b843b18db294f2a2270ae4b7aeafb7da8d5bf9
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-10-28 19:22:03 +0100

    Extract `format_error()` function

--- pytest_voluptuous/plugin.py
+++ pytest_voluptuous/plugin.py
@@ -12,10 +12,14 @@ def pytest_assertrepr_compare(op, left, right):
         else:
             source = right
         if isinstance(source.error, MultipleInvalid):
-            errors = ['- {}: {}'.format('.'.join(map(str, error.path)), error) for error in source.error.errors]
+            errors = [format_error(error) for error in source.error.errors]
         else:
-            errors = ['- {}: {}'.format('.'.join(map(str, source.error.path)), source.error)]
+            errors = [format_error(source.error)]
         return [
             'failed to validation error(s):'
         ] + errors
     return None
+
+
+def format_error(error):
+    return '- {}: {}'.format('.'.join(map(str, error.path)), error)
